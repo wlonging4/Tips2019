5041a12823e17c407883657107c3fb508d728679
吴龙广 <longguangwu@creditease.cn>
2019-06-18 12:34:24 +0800

    添加2018 creditease

---
+++ file2018/1806/20180612_私人定制产品前端原型及文档/20180612_私人定制产品前端原型及文档/理财师_私人定制产品线上原型_20180612/files/类固定收益/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j],k,_(l,m,n,o,p,q,r,_(),s,_(t,u,v,w,x,_(y,z,A,B),C,null,D,w,E,w,F,G,H,null,I,J,K,L,M,N,O,J),P,_(),Q,_(R,_(S,T,U,[_(S,V,W,g,X,[_(Y,Z,S,ba,bb,_(bc,k,bd,be),bf,bg)])])),bh,_(bi,[])),bj,_(),bk,_());}; 
+var b="url",c="类固定收益.html",d="generationDate",e=new Date(1528785194710.33),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="page",l="packageId",m="5d7237a09b234ea4bb2c8f342d04a9bd",n="type",o="Axure:Page",p="name",q="类固定收益",r="notes",s="style",t="baseStyle",u="627587b6038d43cca051c114ac41ad32",v="pageAlignment",w="near",x="fill",y="fillType",z="solid",A="color",B=0xFFFFFFFF,C="image",D="imageHorizontalAlignment",E="imageVerticalAlignment",F="imageRepeat",G="auto",H="favicon",I="sketchFactor",J="0",K="colorStyle",L="appliedColor",M="fontName",N="Applied Font",O="borderWidth",P="adaptiveStyles",Q="interactionMap",R="onLoad",S="description",T="页面载入时",U="cases",V="Case 1",W="isNewIfGroup",X="actions",Y="action",Z="linkWindow",ba="在 当前窗口 打开 网贷服务",bb="target",bc="targetType",bd="includeVariables",be=true,bf="linkType",bg="current",bh="diagram",bi="objects",bj="masters",bk="objectPaths";
+return _creator();
+})());
